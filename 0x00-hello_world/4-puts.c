52565552eb777df067f2bc7ff38d50fdbdd81856
Avellin003 <uavellinbonaparte@gmail.com>
2023-06-15 09:40:54 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
-
-/**
- * making an executable file
- */
-
-
+/*making an executable file*/
 int main (void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
