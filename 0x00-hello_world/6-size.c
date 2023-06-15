fec34d7a4f2a96e9ae6c3970de48214462be084a
Avellin003 <uavellinbonaparte@gmail.com>
2023-06-15 13:17:34 -0700

    Eddy

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ *printf - send formatted output to the screen
+ *main - serves as the starting point for program execution
+ *return: 0 (success)
+ */
+int main(void)
+{
+	printf("size of char: %d byte(s)\n",sizeof (char);
+	printf("size of int: %d byte(s)\n",sizeof (int));
+	printf("size of long int: %d byte(s)\n",sizeof(long int);
+	printf("size of long long int: %d byte(s)\n",sizeof(long long int));
+	printf("size of float: %d bytes(s)\n",sizeof(float));
+	return (0)
+}
