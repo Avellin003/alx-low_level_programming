a317b6e5526ffa7596986ff5b7d5ba7882ac320e
Avellin003 <uavellinbonaparte@gmail.com>
2023-06-15 12:39:27 -0700

    ee

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,9 @@
 #include<stdio.h>
 
 /**
-
-* main - Entry point
-
-*
-
-* Return: Always 0 (Success)
-
-*/
+ * main -  starting point for program execution
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
