f326d1a9b8cfc69b34547452923222cf43d3ef28
Avellin003 <uavellinbonaparte@gmail.com>
2023-06-15 12:58:24 -0700

    k

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  *printf - send formatted output to the screen
  *Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0)
