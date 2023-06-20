c0bc9a19b39187714dfd78c665d98375e6375913
Avellin003 <uavellinbonaparte@gmail.com>
2023-06-20 09:05:19 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,15 @@
 
 /**
  *print_alphabet - print_low_case_alphabets
- * return: 0
  */
 void print_alphabet(void)
 {
 	char letter;
 
-	for (letter = 'a', letter <= 'z', letter++)
-		_putchar('letter');
-		_putchar('\n');
-	return (0);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+	
 }
 
