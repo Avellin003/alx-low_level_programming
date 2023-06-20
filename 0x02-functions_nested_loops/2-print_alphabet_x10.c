fb1a4e9751e24f4e2c7fd2f3930a9754e7ed1b43
Avellin003 <uavellinbonaparte@gmail.com>
2023-06-20 09:23:33 -0700

    10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet - print_alphabetsx10
+ */
+void print_alphabet_x10(void)
+{
+	char letters;
+		for (letters = 'a'; letters <= 'j'; letters++)
+			_putchar(letters);
+		_putchar('\n');
+}
