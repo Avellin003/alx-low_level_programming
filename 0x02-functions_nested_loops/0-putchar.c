caf9513edf1d627a5c5bade713fb2dae851b3ff4
Avellin003 <uavellinbonaparte@gmail.com>
2023-06-20 08:16:59 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,19 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
-* putchar - writes the character c to stdout
-* retun : -1 (not correct) and/or 1 (correct)
-*/
-char main(void)
+ *main - print _putchar
+ *Return:0
+ */
+int main(void)
 {
-	printf("\_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
