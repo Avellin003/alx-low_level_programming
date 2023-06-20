0e184cff3708a86d9be393b5c21335ae11b84a34
Avellin003 <uavellinbonaparte@gmail.com>
2023-06-20 09:40:17 -0700

    umugabo

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,17 @@
 void print_alphabet_x10(void)
 {
 	char letters;
+	
+	int i=0;
+ 	
+	
+	while (i<10)
+	{
 		for (letters = 'a'; letters <= 'j'; letters++)
+		
 			_putchar(letters);
+	}
 		_putchar('\n');
-}
+
+		i++;
+{
