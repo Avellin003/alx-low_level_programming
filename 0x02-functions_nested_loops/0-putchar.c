04fa09d8bd5b1517ee6b72b60de29a2590aae31c
Avellin003 <uavellinbonaparte@gmail.com>
2023-06-20 04:13:43 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* putchar - writes the character c to stdout
+* retun : -1 (not correct) and/or 1 (correct)
+*/
+char main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
