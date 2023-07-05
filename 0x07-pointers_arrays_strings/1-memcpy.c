d02ca97c380e2a97a664fa69b071c2b646289ebf
Avellin003 <uavellinbonaparte@gmail.com>
2023-07-05 07:11:25 -0700

    Kimba

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+
 /**
  * _memcpy - a function used to copy n bytes to a memory area
  * @n: times to copy
- * @src: variable to copy from
- * @dest: variable to copy to
+ * @src: variable to copy to
+ * @dest: variable to copy from
  * Return: dest
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		src[i] = dest[i];
+		dest[i] = src[i];
 	return (dest);
 }
