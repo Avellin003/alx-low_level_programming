a5bc0f3ad80263cac47d8e06cb381c9e63889bf2
Avellin003 <uavellinbonaparte@gmail.com>
2023-07-05 07:53:42 -0700

    magic

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ *_strchr - used to locate strings
+ *@s: pointer to be return upon occerence of c
+ *@c: loop terminator value
+ *Return: the pointer c on occernce of c or Null for no value at all
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	while (1)
+		{
+			a = *s++;
+			if (a == c)
+			{
+				return (s - 1);
+			}
+			if (a == 0)
+			{
+				return (NULL);
+			}
+		}
+}
