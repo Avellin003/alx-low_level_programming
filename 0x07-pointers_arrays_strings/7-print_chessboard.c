32b23bf22ee8e2bf5db15c1df568ecdc08458102
Avellin003 <uavellinbonaparte@gmail.com>
2023-07-05 12:46:48 -0700

    OK

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: two dimension array to print
+ * _putchar - function used to print 
+ */
+void print_chessboard(char (*a)[8])
+{
+	int j, k;
+
+	for (j = 0; j < 8; j++)
+	{
+		for (k = 0; k < 8; k++)
+		{
+			putchar(a[j][k]);
+		}
+		putchar('\n');
+	}
+}
