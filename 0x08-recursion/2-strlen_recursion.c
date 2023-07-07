4980724d4c59ffb59c8cd6b3e40d1a66cfe29f6b
Avellin003 <uavellinbonaparte@gmail.com>
2023-07-07 13:07:42 -0700

    long

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 /**
  *_strlen_recursion - returns the length of a string
  *@s: string to find the length for
- *return: hte length of the string
+ *Return: the length of the string
  */
 int _strlen_recursion(char *s)
 {
