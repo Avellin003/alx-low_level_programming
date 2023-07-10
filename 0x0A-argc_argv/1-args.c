e1d51a7fbeab139536e8c5565de57893824a97a8
Avellin003 <uavellinbonaparte@gmail.com>
2023-07-10 13:20:46 -0700

    lache

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+
 /**
- * main - prints the number of arguments
- * @argc: argument for count
- * @argv: argument vector
+ * main - prints the number of arguments passed into it.
+ * @argc: argument count
+ * @argv: arguments
+ *
  * Return: 0
  */
 int main(int argc, char **argv)
 {
 	(void)argv;
 
-	printf("%d \n", argc - 1);
-	
+	printf("%d\n", argc - 1);
 	return (0);
 }
