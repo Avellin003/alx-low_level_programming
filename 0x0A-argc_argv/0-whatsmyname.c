a714e6f2fa32117efd8521830fbf97de06d242f1
Avellin003 <uavellinbonaparte@gmail.com>
2023-07-10 12:41:40 -0700

    kumama

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * create a program that prints
+ * it's own name
+ */
+
+int main(int Argc, char *Argv[])
+{
+	(void) Argc;
+
+	printf("Argv : %s\n", Argv[0]);
+	return (0);
+}
