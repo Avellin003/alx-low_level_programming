0bcf6aed5de144f02e4ae9fbbe5828cb52e2d11a
Avellin003 <uavellinbonaparte@gmail.com>
2023-07-10 15:03:16 -0700

    j

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the min number of coins to make change
+ * for an amount of money
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int T,C;
+	unsigned int x;
+	char *p;
+	int cents[] = {25, 10, 5, 2};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	T = strtol(argv[1], &p, 10);
+	C = 0;
+
+	if (!*p)
+	{
+		while (T > 1)
+		{
+			for (x = 0; x < sizeof(cents[x]); x++)
+			{
+				if (T >= cents[x])
+				{
+					C += T / cents[x];
+					T = T % cents[x];
+				}
+			}
+		}
+		if (T == 1)
+			C++;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", C);
+	return (0);
+}
