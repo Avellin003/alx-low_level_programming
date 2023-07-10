3713162ab7f5d4190c80bed0dd7c11c0aa7f8594
Avellin003 <uavellinbonaparte@gmail.com>
2023-07-10 14:54:57 -0700

    Chineke

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,24 +2,37 @@
 #include <stdlib.h>
 
 /**
- * main - multiplies two numbers.
+ * main - adds positive numbers.
  * @argc: argument count
- * @argv: argument vector
+ * @argv: arguments
+ *
  * Return: 0
  */
 int main(int argc, char **argv)
 {
-        int i, j;
+	int a, n, sum = 0;
+	char *f;
 
-        if (argc < 3)
-        {
-                printf("Error\n");
-                return (1);
-        }
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-        i = atoi(argv[1]);
-        j = atoi(argv[2]);
-        printf("%d\n", i + j);
+	for (a = 1; argv[a]; a++)
+	{
+		n = strtol(argv[a], &f, 10);
+		if (*f)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
 
-        return (0);
+	return (0);
 }
