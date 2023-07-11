fb14a19bb333f83689240da9a76bb56376c46f6c
Avellin003 <uavellinbonaparte@gmail.com>
2023-07-11 05:56:33 -0700

    yuhu

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,41 +6,42 @@
  * str_concat - concatenates two strings.
  * @s1: first string
  * @s2: second string
+ *
  * Return: a pointer to a newly allocated space in memory which
  * contains the contents of s1, followed by the contents of s2,
  * and null terminated. NULL on failure
  */
 char *str_concat(char *s1, char *s2)
 {
-	int a, b, n1, n2, n;
-	char *Result;
+	int i, j, av1, av2, av;
+	char *result;
 
-	n1 = n2 = 0;
+	av1 = av2 = 0;
 
 	if (s1 != NULL)
 	{
-		a = 0;
-		while (s1[a++] != '\0')
-			n1++;
+		i = 0;
+		while (s1[i++] != '\0')
+			av1++;
 	}
 
 	if (s2 != NULL)
 	{
-		a = 0;
-		while (s2[a++] != '\0')
-			n2++;
+		i = 0;
+		while (s2[i++] != '\0')
+			av2++;
 	}
 
-	n = n1 + n2;
-	Result = (char *)malloc(sizeof(char) * (n + 1));
-	if (Result == NULL)
+	av = av1 + av2;
+	result = (char *)malloc(sizeof(char) * (av + 1));
+	if (result == NULL)
 		return (NULL);
 
-	for (a = 0; a < n1; a++)
-		Result[a] = s1[a];
-	for (b = 0; b < n2; b++, b++)
-		Result[a] = s2[b];
-	Result[n] = '\0';
+	for (i = 0; i < av1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < av2; j++, i++)
+		result[i] = s2[j];
+	result[av] = '\0';
 
-	return (Result);
+	return (result);
 }
