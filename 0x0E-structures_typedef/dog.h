d01fe161a5a8e8e083e6b04bbc5dc513fce68f79
Avellin003 <uavellinbonaparte@gmail.com>
2023-07-17 05:22:01 -0700

    d

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ *struct dog - prints dog's id
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: dog's owner
+ */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog;
+} dog;
 #endif
