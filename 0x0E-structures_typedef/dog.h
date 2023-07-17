f03115bcf57a7f8b06fcdb677a013ba786e9c763
Avellin003 <uavellinbonaparte@gmail.com>
2023-07-17 09:40:42 -0700

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
-#define DOG_H
-void init_dog(struct dog *d, char *name, float age, char *owner);
+#define DOG_
 /**
  *struct dog - prints dog's id
  *@name: dog's name
@@ -13,4 +12,14 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
 #endif
