e6fd059f836e31921e664ea1c095ff2d7ae1bb21
Avellin003 <uavellinbonaparte@gmail.com>
2023-07-17 03:05:20 -0700

    preprocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define AVELLIN printf(__FILE__)
+/**
+ * main - prints the file name
+ *Return: 0 when success
+ */
+int main(void)
+{
+	AVELLIN;
+	printf("\n");
+	return (0);
+}
