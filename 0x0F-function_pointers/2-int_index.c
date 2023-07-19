255f1a03b6840964197a4bb62e369b94dcc752eb
Avellin003 <uavellinbonaparte@gmail.com>
2023-07-19 12:54:59 -0700

    yuhu

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,10 +1,12 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * cmp - function that compares
- * @size: number of elements in array
- * return: returns 0 for unsuccess and -1 for a negative number
- */
+  * int_index - function indexes
+  * @array: pointer argument
+  * @size: argument
+  * @cmp: function pointer
+  * Return: -1
+  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
