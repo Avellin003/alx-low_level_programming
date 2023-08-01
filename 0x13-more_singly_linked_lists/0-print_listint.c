e13f767d68dd5e932dab8f529ac5825080b23540
Avellin003 <uavellinbonaparte@gmail.com>
2023-08-01 04:26:38 -0700

    listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,16 +1,17 @@
 #include "lists.h"
 #include <string.h>
 #include <stdio.h>
-
+#include <stddef.h>
 /**
  * print_listint - prints elements
+ * @num: number of nodes
  * @h: pointer used access data in listint_t
  * Return: size_t
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
-	
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
