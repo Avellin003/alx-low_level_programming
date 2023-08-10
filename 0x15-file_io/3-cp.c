9482001a136d26aae74e9413cc6c22f4f4cbf67a
Avellin003 <uavellinbonaparte@gmail.com>
2023-08-10 05:22:55 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * check_arg - function that checks the number of args
+ * check_num - evaluates the num of args
  * @argc: argument count
  * Return: nothing
  */
-void check_arg(int argc)
+void check_num(int argc)
 {
 	if (argc != 3)
 	{
@@ -16,45 +16,43 @@ void check_arg(int argc)
 }
 
 /**
- * check_file_r - function that checks the readability
- * @assess: argument
- * @file:file to be checked
- * @file_from: argument
- * Return: void
+ * check_exist - evaluates whether the function exist
+ * @assess: views either true or false
+ * @file: argument
+ * @fd_from: argument
+ * @fd_to: argument
+ * Return: nothing
  */
-void check_file_r(ssize_t assess, char *file, int file_from)
+void check_exist(ssize_t assess, char *file, int fd_from, int fd_to)
 {
-	int file_to;
-
 	if (assess == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
-		if (file_from != -1)
-			close(file_from);
-		if (file_to != -1)
-			close(file_to);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
 		exit(98);
 	}
 }
 
 /**
- * check_w - function
+ * check_file - function
  * @assess: evaluates whether true or false
  * @file: argument
+ * @fd_from: argument
+ * @fd_to: argument
  * Return: nothing
  */
-void check_w(ssize_t assess, char *file)
+void check_file(ssize_t assess, char *file, int fd_from, int fd_to)
 {
-	int file_to;
-	int file_from;
-
 	if (assess == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-		if (file_from != -1)
-			close(file_from);
-		if (file_to != -1)
-			close(file_to);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
 		exit(99);
 	}
 }
@@ -62,49 +60,49 @@ void check_w(ssize_t assess, char *file)
 /**
  * check_close - function
  * @assess: evaluates whether true or false
- * @filedescription: argument
+ * @fd: argument
  * Return: nothing
  */
-void check_close(int assess, int filedescription)
+void check_close(int assess, int fd)
 {
 	if (assess == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", filedescription);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
 /**
- * main - running function
+ * main -running function
  * @argc: argument count
  * @argv: argument vector
  * Return: 0 if success
  */
 int main(int argc, char *argv[])
 {
-	int filedescription_from, filedescription_to, cl_to, cl_from;
-	ssize_t lengthr, lenghtw;
+	int fd_from, fd_to, close_to, close_from;
+	ssize_t lenr, lenw;
 	char holder[1024];
-	mode_t file_p;
+	mode_t file_perm;
 
-	check_arg(argc);
-	filedescription_from = open(argv[1], O_RDONLY);
-	check_file_r((ssize_t)filedescription_from, argv[1], -1, -1);
-	file_p = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	filedescription_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_p);
-	check_w((ssize_t)filedescription_to, argv[2], filedescription_from, -1);
-	lenghtnr = 1024;
-	while (lenghtr == 1024)
+	check_num(argc);
+	fd_from = open(argv[1], O_RDONLY);
+	check_exist((ssize_t)fd_from, argv[1], -1, -1);
+	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
+	check_file((ssize_t)fd_to, argv[2], fd_from, -1);
+	lenr = 1024;
+	while (lenr == 1024)
 	{
-		lenghtr = read(filedescription_from, holder, 1024);
-		check_file_r(lenghtr, argv[1], filedescription_from, filedescription_to);
-		lenghtw = write(filedescription_to, holder, lenghtr);
-		if (lengthw != lengthr)
-			lengthw = -1;
-		check_w(lenghtw, argv[2], filedescription_from, filedescription_to);
+		lenr = read(fd_from, holder, 1024);
+		check_exist(lenr, argv[1], fd_from, fd_to);
+		lenw = write(fd_to, holder, lenr);
+		if (lenw != lenr)
+			lenw = -1;
+		check_file(lenw, argv[2], fd_from, fd_to);
 	}
-	cl_to = close(filedescription_to);
-	cl_from = close(filedescription_from);
-	check_close(close_to, filedescription_to);
-	check_close(close_from, filedescription_from);
+	close_to = close(fd_to);
+	close_from = close(fd_from);
+	check_close(close_to, fd_to);
+	check_close(close_from, fd_from);
 	return (0);
 }
