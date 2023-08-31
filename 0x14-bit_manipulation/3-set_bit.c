bd07a44e4da873cdcb945ce3ef37e0d32756e5f7
Avellin003 <uavellinbonaparte@gmail.com>
2023-08-31 04:12:37 -0700

    update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * set_bit - sets value of bit to 1 at specific places 
- * @index: is the bit to change 
+ * set_bit - sets value of bit to 1 at specific places
+ * @index: is the bit to change
  * @n: the holder of bianry members
  * Return: 1 if success or -1 if fail
  */
@@ -9,7 +9,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int avellin;
 
-	if (index > (sizeof(unsigned long int) * 8 - 1))
+	if (index > (sizeof(unsigned long int) * 7))
 		return (-1);
 	avellin = 1 << index;
 	*n = *n | avellin;
