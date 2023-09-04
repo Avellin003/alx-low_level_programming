0d7aa9f2c2cd331faac333949bc916864a97f951
Avellin003 <uavellinbonaparte@gmail.com>
2023-09-04 03:36:36 -0700

    0-read_textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * read_textfile - function that reads
+ * @filename: A pointer argument
+ * @letters: argument
+ * Return: if success return text else 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t y, z, a;
+	char *hold;
+
+	if (filename == NULL)
+		return (0);
+
+	hold = malloc(sizeof(char) * letters);
+	if (hold == NULL)
+		return (0);
+
+	y = open(filename, O_RDONLY);
+	z = read(y, holder, letters);
+	a = write(STDOUT_FILENO, holder, z);
+
+	if (y == -1 || z == -1 || a == -1 || a != z)
+	{
+		free(hold);
+		return (0);
+	}
+
+	free(hold);
+	close(y);
+
+	return (a);
+}
