44bd896a2ce1a0ce6df5944774a59b1b3fa2f807
root <root@avellin.localdomain>
2024-02-07 11:26:52 +0200

    updates

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,8 +2,11 @@
 #define SEARCH_ALGOS_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 /*0-linear.c*/
 int linear_search(int *array, size_t size, int value);
 /*1-binary.c*/
 int binary_search(int *array, size_t size, int value);
+/*100-jump.c*/
+int jump_search(int *array, size_t size, int value);
 #endif
